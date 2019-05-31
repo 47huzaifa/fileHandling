084caf497d4048c2e2d648c3ab845e33375bad6a
47huzaifa <huzaifa.pps.ta@gmail.com>
2019-05-31 06:08:35 -0700

    take input

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -1,5 +1,19 @@
+import Domain.Employee;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input;
+        input = new Scanner(System.in);
+        Employee employee = new Employee();
+
+        System.out.println("Enter Your Name ");
+        employee.setName(input.next());
+        System.out.println("Enter Your cnic ");
+        employee.setCnic(input.next());
+        System.out.println("Enter Your Age ");
+        employee.setAge(input.nextInt());
 
 
     }
